e2bfc73e71ae3abdbaa206c531d4663f30c36305
zumbost1k <matusevich.mi@gmail.com>
2023-12-14 10:14:08 +0300

    add mongo

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 import express from 'express';
-const PORT = 5000;
+import mongoose from 'mongoose';
+import Post from '../schemas/Post';
 
-import mongoose from 'mongoose'; // Correct import
-const dbURL =
-  'mongodb+srv://user:user@misha.ojl6144.mongodb.net/?retryWrites=true&w=majority';
+const PORT = 5000;
+const dbURL = 'mongodb://127.0.0.1:27017/clocking_system';
 const app = express();
 
 //преобразовываем данные из запроса из json в js
 app.use(express.json());
-
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.status(200).json('сервер работает нормально123');
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!');
 });
 
 async function startApp() {
   try {
-    await mongoose.connect(dbURL);
+    await mongoose.connect(dbURL, {});
     app.listen(PORT, () => {
       console.log('server started on port ' + PORT);
     });
